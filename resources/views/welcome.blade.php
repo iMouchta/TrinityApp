4dcc5e0a6b014d29ac0644edded48588c67c7e62
Jonathan Peralta Flores <jonathanperaltaflores@gmail.com>
2023-10-03 19:09:52 -0400

    Version inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/plantilla')
+
+@section('tituloPestana','primera version')
+
+@section('contenido')
+    <div class="row">
+        <h1>
+            TrinityApp
+        </h1>
+    </div>
+@endsection
