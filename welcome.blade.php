3d9bbb0db61245cc65c5be75960b06883b97cd09
iMouchta <146895731+iMouchta@users.noreply.github.com>
2023-10-06 12:50:38 -0400

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/plantilla')
+
+@section('tituloPestana','primera version')
+
+@section('contenido')
+    <div class="row">
+        <h1>
+            Gestor de eventos
+        </h1>
+    </div>
+@endsection
