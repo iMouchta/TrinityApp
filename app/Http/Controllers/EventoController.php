4afe42d8b9c59b3ce0350bd7385f0f27f5006395
Jonathan Peralta Flores <jonathanperaltaflores@gmail.com>
2023-10-09 03:12:23 -0400

    creacion controllers y models

---
+++ app/Http/Controllers/EventoController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Evento;
+use Illuminate\Http\Request;
+
+class EventoController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Evento $evento)
+    {
+        //
+    }
+
+ 
+    public function edit(Evento $evento)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Evento $evento)
+    {
+        //
+    }
+
+ 
+    public function destroy(Evento $evento)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',[EventoController::class, 'index'])->name('Evento.index');
+Route::get('/create',[EventoController::class, 'create'])->name('Evento.create');
