c6356915aa9b7a8371a9906c0263ae821d04c233
Dafne-Cruz <135438200+Dafne-Cruz@users.noreply.github.com>
2023-10-09 08:02:42 -0400

    Validación formulario

---
+++ app/Http/Requests/eventoRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class eventoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'titulo' => 'required|regex:/^[a-zA-Z\s áéíóúÁÉÍÓÚñÑ]+$/u',
+            'descripcion' => 'required|regex:/^[a-zA-Z\s áéíóúÁÉÍÓÚñÑ]+$/u',
+            
+            'fecha_inicio' => 'required|fecha|after_or_equal:now',
+            
+            'duracion'=>'required',
+            'competencia'=>'in:grupal,individual',
+            
+            'requisitos'=>'required|string',
+            'direccion'=>'required|string',
+
+            'foto'=>'required',
+            'contactos'=> 'required|regex:/^[0-9 ,]+$/u',
+
+            'afiche'=>'required|file|image',
+            'costo'=>'required|numeric|decimal:2|min:0'
+        ];
+    }
+}
