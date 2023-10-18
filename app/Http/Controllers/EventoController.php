62941b51c3baf0e6e0651e064f03c572eec8b1e9
Roger Espinoza <rudy800spinoza@gmail.com>
2023-10-17 23:40:26 -0400

    add subir archivo

---
+++ app/Http/Controllers/EventoController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Evento;
+use Illuminate\Http\Request;
+
+class EventoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Evento $evento)
+    {
+        //
+    }
+
+    public function edit(Evento $evento)
+    {
+        //
+    }
+
+    public function update(Request $request, Evento $evento)
+    {
+        //
+    }
+
+    public function destroy(Evento $evento)
+    {
+        //
+    }
+}
