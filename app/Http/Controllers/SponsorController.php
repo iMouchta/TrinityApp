9d80a084304abbfca1acf1b4b820455900f87f94
Jonathan Peralta Flores <jonathanperaltaflores@gmail.com>
2023-11-12 05:44:28 -0400

    bugSponsor

--- app/Http/Controllers/SponsorController.php
+++ app/Http/Controllers/SponsorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Sponsor;
 use App\Models\Evento;
+use Illuminate\Support\Str;
 
 class SponsorController extends Controller
 {
@@ -16,17 +17,18 @@ class SponsorController extends Controller
 
     public function guardarSponsor(Request $request)
     {
-        // $request->validate([
-        //     'SPONSOR_NOMBRE' => 'required',
-        //     'SPONSOR_ENLACE' => 'nullable|url',
-        //     'SPONSOR_LOGO' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-        // ]);
-
         
         $sponsor = new Sponsor;
         $sponsor->SPONSOR_NOMBRE = $request->input('SPONSOR_NOMBRE');
         $sponsor->SPONSOR_ENLACE = $request->input('SPONSOR_ENLACE');
-        $sponsor->SPONSOR_LOGO = null;
+
+        if ($request->hasFile('SPONSOR_LOGO')) {
+            $logoFile = $request->file('SPONSOR_LOGO');
+            $nombreLogo = Str::slug($sponsor->SPONSOR_NOMBRE) . '-logo.' . $logoFile->getClientOriginalExtension();
+            $rutaLogo = $logoFile->storeAs('logosSponsor', $nombreLogo, 'local');
+            $sponsor->SPONSOR_LOGO = $rutaLogo;
+        }
+
         $sponsor->save();
 
         return redirect()->route('sponsor')->with('success', 'Sponsor guardado correctamente.');
