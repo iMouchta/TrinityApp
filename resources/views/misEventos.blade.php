fdd73753a9eb0b33ee9ce32255276b52fe304529
cesar <cesarcrac44@gmail.com>
2023-11-13 16:50:41 -0400

    css

--- resources/views/misEventos.blade.php
+++ resources/views/misEventos.blade.php
@@ -16,20 +16,24 @@
     @foreach($eventos as $evento)
     <div class="col-md-3">
             <div class="cardevento">
-                <div class="card">
+                <div class="card w-100">
                     <div class="card-body">
+
                         <div class="titulo">
                             <h2 class="text-center">{{ $evento->EVENTO_NOMBRE }}</h2>
                         </div>
-                            <p>Tipo de Evento: {{ $evento->EVENTO_TIPO }}</p>
+                            <p class="letras ">Tipo de Evento : {{ $evento->EVENTO_TIPO }}</p>
 
                                         @foreach($evento->fechas->take(2) as $fecha)
-                                            <p class="crono">{{ $fecha->FECHA_NOMBRE }} : {{ $fecha->FECHA_FECHA }}</p>
+                                            <p class="letras">{{ $fecha->FECHA_NOMBRE }} : {{ $fecha->FECHA_FECHA }}</p>
                                         @endforeach
-                                                    
-                            <center><button type="button" class="btn btn-primary" onclick="()">Ver evento</button></center>
-                            <!-- Mostrar más información del evento según tus necesidades -->
+                                       
                         </div>
+
+                        <div class=" botonver ">
+                                        <center> <button type="button" class="btn btn-primary" onclick="()">Ver evento</button></center>
+                            <!-- Mostrar más información del evento según tus necesidades -->
+                            </div>
                     </div>      
                 </div>
             <br>
