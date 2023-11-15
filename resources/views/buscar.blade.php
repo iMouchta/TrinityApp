c36bb6601d01879b4f50b0b9d52c07f2e4313c97
cesar <cesarcrac44@gmail.com>
2023-11-15 05:46:52 -0400

    b

--- resources/views/buscar.blade.php
+++ resources/views/buscar.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.plantilla')
 
 @section('buscar')
+<link rel="stylesheet" href="{{ asset('css/caja.css') }}">
     <div class="container">
         <h1>Resultados de la búsqueda</h1>
 
@@ -8,10 +9,30 @@
             <ul>
                 @foreach ($eventos as $evento)
                     <li>
-                        <strong>Nombre:</strong> {{ $evento->EVENTO_NOMBRE }} <br>
-                        <strong>Tipo:</strong> {{ $evento->EVENTO_TIPO }} <br>
-                        <strong>Modalidad:</strong> {{ $evento->EVENTO_MODALIDAD }} <br>
-                        <strong>Costo:</strong> {{ $evento->EVENTO_COSTO }} <br>
+                    <div class="col-md-3">
+            <div class="cardevento">
+                <div class="card w-100">
+                    <div class="card-body">
+                        <div class="titulo">
+                            <h2 class="text-center">{{ $evento->EVENTO_NOMBRE }}</h2>
+                        </div>
+                            <p class="letras ">Tipo de Evento : {{ $evento->EVENTO_TIPO }}</p>
+
+                                       @foreach($evento->fechas->take(2) as $fecha)
+                                            <p class="letras">{{ $fecha->FECHA_NOMBRE }} : {{ $fecha->FECHA_FECHA }}</p>
+                                        @endforeach
+                                       
+                        </div>
+
+                        <div class=" botonver ">
+                        <center><a href="{{ route('ver.evento' , ['evento' => $evento->EVENTO_ID]) }}" class="btn btn-primary">Ver evento</a></center>
+                            <!-- Mostrar más información del evento según tus necesidades -->
+                            </div>
+                    </div>      
+                </div>
+            <br>
+
+        </div>
                     </li>
                 @endforeach
             </ul>
