b4dfe47097db7c3c1c9daaf6ec685a4f7d088175
henrryllanos <henrryllanos2022@gmail.com>
2023-12-12 16:48:34 -0400

    presentacion

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -6,13 +6,57 @@
 <head>
     <meta charset="UTF-8">
     <title>Enviar Correo</title>
+    <style>
+        body {
+          font-family: Arial, sans-serif;
+          text-align: center;
+          background-color: #f8f8f8;
+        }
+        .container {
+          max-width: 600px;
+          margin: 0 auto;
+          padding: 20px;
+          border: 1px solid #ccc;
+          border-radius: 8px;
+          background-color: #fff;
+        }
+        .logo {
+          max-width: 200px;
+          margin: 0 auto 20px;
+        }
+        button {
+          display: inline-block;
+          padding: 10px 20px;
+          font-size: 16px;
+          background-color: #3498db;
+          color: #fff;
+          text-decoration: none;
+          border-radius: 5px;
+          transition: background-color 0.3s;
+        }
+        button:hover {
+          background-color: #2980b9;
+        }
+      </style>
 </head>
 
 <body>
     <p>{{ $details['body'] }}</p>
+
+    <div class="container">
+        <img src="https://i.ibb.co/F5FN7ZP/logo.png" alt="logo" border="0"  alt="Logo de la organización" class="logo"/>
+        <h2>¡Bienvenido al Evento de Programación!</h2>
+        <p>Estamos emocionados de tenerte con nosotros. Únete a nosotros para aprender, colaborar y compartir conocimientos sobre programación.</p>
+        <a href="https://drive.google.com/file/d/1dAdh3A0HIzQh6fo0LCaGEYfIoqsS3gx1/view" style="background-color:#1cb0f6;border:1px solid #1cb0f6;border-radius:9px;color:#fff;display:inline-block;font-family:sans-serif;font-size:10px;font-weight:700;letter-spacing:.5px;line-height:42px;padding:0;text-align:center;text-decoration:none;width:280px" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://blast.duolingo.com/redirect/195805?from_email%3D6b6f2f4f8502e88fae8d156968dc55798c53c08cImpvbmF0aGFucGVyYWx0YWZsb3Jlc0BnbWFpbC5jb20i%26user_id%3Dbeb1d55de3accdccc784e44d907a529014666e14NDk1NzM5NjE1%26email%3D6b6f2f4f8502e88fae8d156968dc55798c53c08cImpvbmF0aGFucGVyYWx0YWZsb3Jlc0BnbWFpbC5jb20i%26sample_id%3D8143%26target%3DaHR0cHM6Ly93d3cuZHVvbGluZ28uY29tL3llYXItaW4tcmV2aWV3LzIwMjM/aWQ9OTg2ODg5NTFlZDQ0OTNiMmQ5ZWMyY2RlMWY5MDZlYmQ4OWFlN2QxNjcxNDk2MDE2N2I3ZTYwYjI5YTNjZWQxYSZ2aWE9ZW1haWw%3D&amp;source=gmail&amp;ust=1702434595651000&amp;usg=AOvVaw1dVi41nWr_2ruKxr4PfoLj">SABER MAS SOBRE TRINITY</a>
+        <p>Gracias por ser parte de nuestra comunidad.</p>
+        <p>Saludos,<br>
+        Tu Organización de Eventos de Programación</p>
+      </div>
+
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <a href="https://blast.duolingo.com/redirect/195805?from_email=6b6f2f4f8502e88fae8d156968dc55798c53c08cImpvbmF0aGFucGVyYWx0YWZsb3Jlc0BnbWFpbC5jb20i&amp;user_id=beb1d55de3accdccc784e44d907a529014666e14NDk1NzM5NjE1&amp;email=6b6f2f4f8502e88fae8d156968dc55798c53c08cImpvbmF0aGFucGVyYWx0YWZsb3Jlc0BnbWFpbC5jb20i&amp;sample_id=8143&amp;target=aHR0cHM6Ly93d3cuZHVvbGluZ28uY29tL3llYXItaW4tcmV2aWV3LzIwMjM/aWQ9OTg2ODg5NTFlZDQ0OTNiMmQ5ZWMyY2RlMWY5MDZlYmQ4OWFlN2QxNjcxNDk2MDE2N2I3ZTYwYjI5YTNjZWQxYSZ2aWE9ZW1haWw=" style="background-color:#1cb0f6;border:1px solid #1cb0f6;border-radius:9px;color:#fff;display:inline-block;font-family:sans-serif;font-size:16px;font-weight:700;letter-spacing:.5px;line-height:42px;padding:0;text-align:center;text-decoration:none;width:280px" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://blast.duolingo.com/redirect/195805?from_email%3D6b6f2f4f8502e88fae8d156968dc55798c53c08cImpvbmF0aGFucGVyYWx0YWZsb3Jlc0BnbWFpbC5jb20i%26user_id%3Dbeb1d55de3accdccc784e44d907a529014666e14NDk1NzM5NjE1%26email%3D6b6f2f4f8502e88fae8d156968dc55798c53c08cImpvbmF0aGFucGVyYWx0YWZsb3Jlc0BnbWFpbC5jb20i%26sample_id%3D8143%26target%3DaHR0cHM6Ly93d3cuZHVvbGluZ28uY29tL3llYXItaW4tcmV2aWV3LzIwMjM/aWQ9OTg2ODg5NTFlZDQ0OTNiMmQ5ZWMyY2RlMWY5MDZlYmQ4OWFlN2QxNjcxNDk2MDE2N2I3ZTYwYjI5YTNjZWQxYSZ2aWE9ZW1haWw%3D&amp;source=gmail&amp;ust=1702434595651000&amp;usg=AOvVaw1dVi41nWr_2ruKxr4PfoLj">CONOCE TUS ESTADÍSTICAS</a>
+       
 </body>
 
 </html>
