c655e85089e0cb475a7be57dcdccfa70285d7f3c
Roger Espinoza <rudy800spinoza@gmail.com>
2023-12-12 14:54:09 -0400

    graficos

--- resources/views/barra.blade.php
+++ resources/views/barra.blade.php
@@ -1,7 +1,63 @@
 @extends('layout/plantilla')
 
 @section('barra')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/chart.js">
     <div class="container">
             <h1><center>Grafico de Barras</center></h1>
+
+            <style>
+       body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+        }
+
+        canvas {
+            margin-top: 50px;
+        }
+    </style>
+    <div>
+        <canvas id="myChart" width="400" height="200"></canvas>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                var ctx = document.getElementById('myChart').getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ['Categoría 1', 'Categoría 2', 'Categoría 3', 'Categoría 4', 'Categoría 5'],
+                        datasets: [{
+                            label: 'Frecuencias',
+                            data: [120, 90, 60, 30, 75],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.7)',
+                                'rgba(54, 162, 235, 0.7)',
+                                'rgba(255, 206, 86, 0.7)',
+                                'rgba(75, 192, 192, 0.7)',
+                                'rgba(153, 102, 255, 0.7)',
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            });
+        </script>
+
     </div>
 @endsection
--- resources/views/frecuencia.blade.php
+++ resources/views/frecuencia.blade.php
@@ -1,34 +1,54 @@
 @extends('layout/plantilla')
 
 @section('frecuencia')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/chart.js">
     <div class="container">
-        <h1><center>Grafico de frecuencias</center></h1>
-        <div>
-            <canvas id="myChart"></canvas>
-        </div>
+            <h1><center>Grafico de Barras</center></h1>
+
+            <style>
+       body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+        }
+
+        canvas {
+            margin-top: 50px;
+        }
+    </style>
+    <div>
+        <canvas id="myChart" width="400" height="200"></canvas>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
-        const ctx = document.getElementById('myChart');
-
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
+        document.addEventListener('DOMContentLoaded', function () {
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: ['Categoría 1', 'Categoría 2', 'Categoría 3', 'Categoría 4', 'Categoría 5'],
+                    datasets: [{
+                        label: 'Frecuencias',
+                        data: [120, 90, 60, 30, 75],
+                        fill: true,
+                        borderColor: '#3498db',
+                        borderWidth: 2,
+                        pointRadius: 5,
+                        pointBackgroundColor: '#3498db',
+                        pointBorderColor: 'transparent',
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        });
     </script>
+
+    </div>
 @endsection
